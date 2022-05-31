970ae579e371be6f4b8372b9f34143899bfe6221
unknown <fatihabenou2021@gmail.com>
2022-05-31 10:52:52 +0200

    update de la modif index.js

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 // use micro framework express
 const express = require('express');
 
+// use cors 
+const cors = require('cors');
+
 // use axios
 const axios = require('axios');
 
 // use a data file
-const data = require('./data.json');
+//const data = require('./data.json');
 
 // use port 8000 to run server on localhost
 const port = 8000;
 
 // initialize express in a variable named app
-const app = express();
+const server = express();
 
 // configure express to use urlencoded
-app.use(express.urlencoded({
+server.use(express.urlencoded({
     extended: true
 }));
 
-// log server start
-app.listen(port, () => {
+// configure cors
+server.use(cors('*'));
+
+// default entry point '/' 
+server.get('/', (req, res) => {
+    res.json({ message : 'Welcome on Express/Node Server'}).status(200);
+});
+
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
